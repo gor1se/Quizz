aa3afe7ec9a7b6d4428ec2e42c729045bbb9ca4a
Andreas Schmidt <andreas.schmidt.lth@t-online.de>
2021-11-14 15:21:46 +0100

    Zwischenspeicherfunktion erstellt. Funktioniert bereits bei dem Raumnamen und Passwort.t . Am Ende soll das gesamte Objekt einmal gespeichert werden und anschließend bei der HTML Methode mit value aufgebaut werden. Funktioniert bei dem Objekt noch nicht

--- public/JS/form.js
+++ public/JS/form.js
@@ -17,14 +17,14 @@ let form_obj_to_html = function(obj) {
     let s = "";
     let v = 1;
     let answers_txt_indx = 1;
-    s += "<hr><input type='text' name='room_name' id='room_name' placeholder='Room Name'><br>";
-    s += "<input type='password' name='room_password' id='room_password' placeholder='Room Password'><hr>"
+    s += "<hr><input type='text' name='room_name' id='room_name' placeholder='Room Name' value='" + obj.name + "'><br>";
+    s += "<input type='password' name='room_password' id='room_password' placeholder='Room Password' value='" + obj.password + "'><hr>"
     obj.questions.forEach(element => {
         answers_txt_indx = 1;
-        s += "<h3>" + element.question + "</h3>"; // Heading
-        s += "<textarea name='q_" + v + "' id='question_text_" + v + "' cols='100' rows='1' placeholder='Question'></textarea>"; // Question
+        s += "<h3>Question" + v + "</h3>"; // Heading
+        s += "<textarea name='q_" + v + "' id='q_" + v + "' cols='100' rows='1' placeholder='Question'></textarea>"; // Question
         element.answers.forEach(answer => {
-            s += "<textarea name='q_" + v + "_a_" + answers_txt_indx + "' id='answer_text_ " + answers_txt_indx + "' cols='100' rows='1' placeholder='Answeroption'></textarea>";
+            s += "<textarea name='q_" + v + "_a_" + answers_txt_indx + "' id='q_" + v + "_a_" + answers_txt_indx + "' cols='100' rows='1' placeholder='Answeroption'></textarea>";
             answers_txt_indx++;
         });
         s += "<br><button type='button' class='btn btn-success' onClick='add_answer(this.id)' id='input_" + v + "'>+</button>";
@@ -48,10 +48,14 @@ let form_obj_to_html = function(obj) {
 document.getElementById("form-content").innerHTML = form_obj_to_html(form_obj);
 
 let add_answer = function(id) {
+    // Überprüfe die Werte der eingegebenen Felder und speichere sie ab
+    save_values();
     index = parseInt(id.substring(6));
     let x = "Answer " + (parseInt(form_obj.questions[index - 1].answers.length) + 1);
     form_obj.questions[index - 1].answers.push(x);
     document.getElementById("form-content").innerHTML = form_obj_to_html(form_obj);
+    // Set Value am besten in der form_obj_to_html funktion
+    document.getElementById("room_name").value = form_obj.name;
 }
 
 let remove_answer = function(id) {
@@ -72,4 +76,25 @@ let add_question = function() {
 let remove_question = function() {
     form_obj.questions.pop();
     document.getElementById("form-content").innerHTML = form_obj_to_html(form_obj);
+}
+
+let save_values = function() {
+    form_obj.name = document.getElementById("room_name").value;
+    form_obj.password = document.getElementById("room_password").value;
+    // ID's müssen selbst gebaut werden
+    // Für jede Frage speichere die Frage ab 
+    let question_count = 1;
+    form_obj.questions.forEach(element => {
+        element.question = document.getElementById("q_" + question_count);
+        // Hier folgt nächste forEach um die Antworten zu speichern
+        let answer_count = 1;
+        element.answers.forEach(answer => {
+            answer = document.getElementById("q_" + question_count + "_a_" + answer_count);
+            answer_count++;
+        })
+        question_count++;
+    });
+    // Überprüfen ob das Objekt richtig abgespeichert wird!
+    //console.log(form_obj.questions.question[0]);
+    // Für jede Antwort auf die Frage speichere die Antwort ab
 }
